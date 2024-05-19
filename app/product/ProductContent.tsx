3fb02fd0e01f6cff0d41f8bc2cf49dc1a52d83f0
prasojopxl <prasojodesigner@gmail.com>
2024-05-19 22:33:31 +0700

    sort product

--- app/product/ProductContent.tsx
+++ app/product/ProductContent.tsx
@@ -1,13 +1,43 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import Box from "@/components/Box"
 import { ItemCategory, ItemInfo } from "./ItemCategory"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import ItemProduct from "@/components/product/ItemProduct"
+import { useQuery } from "@tanstack/react-query"
+import Skeleton from "@/components/Skeleton";
+import { getData } from "@/lib/services";
+import ErrorNetwork from "@/components/ErrorNetwork";
+import _ from "lodash";
 
 export default function ProductContent() {
+    const [sortDataBy, setSortDataBy] = useState("updated_at")
+    const [sortDesc, setSortDesc] = useState(true)
+    const getQuery = async () => {
+        return await getData(`/products`)
+    }
+    const query = useQuery({
+        queryKey: ["qProducts"],
+        queryFn: getQuery
+    })
+    if (query.isLoading) {
+        return (
+            <div className=" relative flex justify-center ">
+                <Skeleton width="100%" height="150px" spaceBottom={"10px"} />
+            </div>
+        )
+    }
+
+    if (query.isError) {
+        return (
+            <ErrorNetwork />
+        )
+    }
+    const dataContent = query?.data?.data.data
+    const dataSort = _.orderBy(dataContent, [`${sortDataBy}`], [`${sortDesc ? "desc" : "asc"}`]);
+
     return (
         <div className="flex justify-between items-start gap-7">
             <Box className="bg-c-blue text-white max-w-[280px]">
@@ -62,9 +92,13 @@ export default function ProductContent() {
                         <div className="flex  items-center gap-4">
                             <label className="mr-3">Urutkan</label>
                             <Button variant="outline">Terkait</Button>
-                            <Button variant="outline">Terbaru</Button>
-                            <Button variant="outline">Terlaris</Button>
-                            <Button variant="outline" className="flex gap-1 items-center">Harga <IoIosArrowUp /></Button>
+                            <Button variant="outline" onClick={() => { setSortDataBy("updated_at") }}>Terbaru</Button>
+                            <Button variant="outline" onClick={() => { setSortDataBy("sold_count") }}>Terlaris</Button>
+                            <Button variant="outline" onClick={() => {
+                                setSortDataBy("price")
+                                setSortDesc(!sortDesc)
+
+                            }} className="flex gap-1 items-center">Harga {sortDesc ? <IoIosArrowDown /> : <IoIosArrowUp />}</Button>
                             <div className="flex items-center gap-2">
                                 Rp <Input placeholder="Harga Minimum" />
                             </div>
@@ -76,46 +110,23 @@ export default function ProductContent() {
                 </div>
                 <Box className="mt-3">
                     <div className="flex flex-wrap  -mx-2">
-                        <ItemProduct
-                            url="/"
-                            key={1}
-                            title="ProductName"
-                            image_url="/images/pic.png"
-                            price="200.000"
-                            rate="4"
-                            sold="10"
-                            location={"unknown"}
-                        />
-                        <ItemProduct
-                            url="/"
-                            key={1}
-                            title="ProductName"
-                            image_url="/images/pic.png"
-                            price="200.000"
-                            rate="4"
-                            sold="10"
-                            location={"unknown"}
-                        />
-                        <ItemProduct
-                            url="/"
-                            key={1}
-                            title="ProductName"
-                            image_url="/images/pic.png"
-                            price="200.000"
-                            rate="4"
-                            sold="10"
-                            location={"unknown"}
-                        />
-                        <ItemProduct
-                            url="/"
-                            key={1}
-                            title="ProductName"
-                            image_url="/images/pic.png"
-                            price="200.000"
-                            rate="4"
-                            sold="10"
-                            location={"unknown"}
-                        />
+                        {
+                            dataSort?.map((item: any) => {
+                                return (
+                                    <ItemProduct
+                                        url={`/product/${item.id}`}
+                                        key={item.id}
+                                        title={item.name}
+                                        image_url={item.photos[0].image_url}
+                                        price={item.price}
+                                        rate={parseInt(item.average_rating).toFixed(1)}
+                                        sold={item.sold_count}
+                                        location={item.vendor_region ? item.vendor_region : "unknown"}
+                                    />
+                                )
+                            })
+                        }
+
                     </div>
                 </Box>
             </div>
