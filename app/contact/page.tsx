80dcee62d4dc97b67bbb569856f7b6b1ceb182fa
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-05-20 13:28:38 +0700

    create contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,89 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const TopContactPage = () => {
+  return (
+    <div className="bg-[url('/images/contact.png')] w-full h-screen bg-no-repeat bg-cover">
+      <div className="wrapper py-16">
+        <div className="flex gap-2">
+          <p className="text-white">CELEPARTY</p>
+          <div>
+            <Image
+              src={"/images/img-in-contact.png"}
+              width={12}
+              height={32}
+              alt="Img in Contact.."
+            />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <h1 className="font-quick font-semibold text-[56px] leading-[70px] text-white">
+              LET’S HEAR <br /> FROM YOU
+            </h1>
+          </div>
+          <div>
+            <PhoneContact />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center pt-10">
+        <Link href={"/"}>
+          <Image
+            src={"/images/wa-item.png"}
+            width={307}
+            height={94}
+            alt="Wa Image"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const BottomContactPage = () => {
+  return (
+    <div className="bg-c-blue">
+      <div className="wrapper flex justify-center py-20">
+        <p className="text-center text-white font-quick font-semibold text-[16px] leading-[20px]">
+          "Kami memahami betapa pentingnya setiap momen spesial dalam hidupmu.{" "}
+          <br />
+          Itulah alasan mengapa Celeparty hadir untuk mewujudkannya."
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const PhoneContact = () => {
+  return (
+    <div className="font-quick font-bold [&_p]:font-semibold [&_p]:text-[20px] text-[26px] leading-[32px] text-white flex flex-col gap-4">
+      <div>
+        <h1>Mobile Number</h1>
+        <p>+6285721705354</p>
+      </div>
+      <div>
+        <h1>Office Address</h1>
+        <p>
+          jl. Martasik No 10B <br />
+          Cimahi, Jawa Barat, Indonesia
+        </p>
+      </div>
+      <div>
+        <h1>E-mail Address</h1>
+        <p>Celeparty.id@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
 const ContactPage = () => {
-  return <div>CONTACT PAGE</div>;
+  return (
+    <div>
+      <TopContactPage />
+      <BottomContactPage />
+    </div>
+  );
 };
 
 export default ContactPage;
