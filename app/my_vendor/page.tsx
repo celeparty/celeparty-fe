8023391ba00811d1fe9df41a396c97d1bc0591f6
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-06-27 15:10:21 +0700

    finish section right in vendor page

--- app/my_vendor/page.tsx
+++ app/my_vendor/page.tsx
@@ -5,6 +5,21 @@ type Props = {
   title: string;
 };
 
+type ButtonProps = {
+  title: string;
+  count: string;
+  // colorBg: string;
+  className: string;
+};
+
+type ActionItemProps = {
+  src: string;
+  title: string;
+  icon: boolean;
+  status: string;
+  className: string;
+};
+
 const SectionLeftItem = ({ img, title }: Props) => {
   return (
     <div className="flex items-center gap-6 mb-4">
@@ -34,12 +49,219 @@ const SectionLeft = () => {
 };
 
 const SectionRight = () => {
-  return <div className="bg-yellow-500 flex-1">SECTION RIGHT</div>;
+  return (
+    <div className="flex-1">
+      <div className="flex justify-center my-4">
+        <div className="flex gap-10">
+          <ButtonStatus title="PENDING" count="1" className="bg-[#3E2882]" />
+          <ButtonStatus title="PROCCES" count="1" className="bg-[#56C200]" />
+          <ButtonStatus title="CANCEL" count="1" className="bg-[#F60E0E]" />
+          <ButtonStatus
+            title="INCOME"
+            count="Rp.220.000"
+            className="bg-[#44CADC]"
+          />
+        </div>
+      </div>
+      <div>
+        <table className="min-w-full bg-white border-b-2 border-gray-200">
+          <thead className="">
+            <tr>
+              <th className="lg:px-6 px-4 py-3 border-b-4 text-start text-xs font-bold text-[8px] text-black uppercase tracking-wider">
+                ORDER DATE
+              </th>
+              <th className="lg:px-6 px-4 py-3 border-b-4 text-start text-xs font-bold text-[8px] text-black uppercase tracking-wider">
+                ITEM
+              </th>
+              <th className="lg:px-6 px-4 py-3 border-b-4 text-start text-xs font-bold text-[8px] text-black uppercase tracking-wider">
+                STATUS
+              </th>
+              <th className="lg:px-6 px-4 py-3 border-b-4 text-start text-xs font-bold text-[8px] text-black uppercase tracking-wider">
+                TOTAL
+              </th>
+              <th className="lg:px-6 px-4 py-3 border-b-4 text-start text-xs font-bold text-[8px] text-black uppercase tracking-wider">
+                ACTION
+              </th>
+            </tr>
+          </thead>
+          <tbody className="[&_td]:text-start [&_td]:font-normal [&_td]:lg:text-[10px] [&_td]:text-[12px] [&_td]:font-hind [&_td]:text-[#3C3C3C] [&_td]:border-b-4">
+            <tr>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                2023/01/24/1
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Matcha Drip Cake
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Pending
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Rp. 220.000
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                <div className="flex gap-4">
+                  <ActionItem
+                    src="/images/chcek-vendor.png"
+                    title="Process"
+                    icon={true}
+                    className=""
+                    status=""
+                  />
+                  <ActionItem
+                    src="/images/cancel-vendor.png"
+                    title="Cancel"
+                    icon={true}
+                    className=""
+                    status=""
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                2023/01/24/2
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Matcha Drip Cake
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Processing
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Rp. 220.000
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                <div className="flex gap-4">
+                  <ActionItem
+                    src="/images/airline-vendor.png"
+                    title="Shipment"
+                    icon={true}
+                    className=""
+                    status=""
+                  />
+                  <ActionItem
+                    src="/images/cancel-vendor.png"
+                    title="Cancel"
+                    icon={true}
+                    className=""
+                    status=""
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                2023/01/24/3
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Matcha Drip Cake
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Shipping
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Rp. 220.000
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                <ActionItem
+                  src=""
+                  title=""
+                  icon={false}
+                  className="font-hind font-semibold text-[10px] text-[#DA7E01]"
+                  status="Waiting Received Confirmation"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                2023/01/24/3
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Matcha Drip Cake
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Completed
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Rp. 220.000
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                <ActionItem
+                  src=""
+                  title=""
+                  icon={false}
+                  className="font-hind font-semibold text-[10px] text-[#56C200]"
+                  status="COMPLETED"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                2023/01/24/3
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Matcha Drip Cake
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Cancelled
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                Rp. 220.000
+              </td>
+              <td className="lg:px-6 px-4 py-4 border-b border-gray-200">
+                <ActionItem
+                  src=""
+                  title=""
+                  icon={false}
+                  className="font-hind font-semibold text-[10px] text-[#F60E0E]"
+                  status="CANCELLED"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+const ButtonStatus = ({ title, count, className }: ButtonProps) => {
+  return (
+    <div
+      className={`w-[165px] h-[60] text-white px-10 py-4 [&_p]:text-center [&_h1]:text-center rounded-xl ${className}`}
+    >
+      <h1 className="font-hind font-normal text-[12px]">{title}</h1>
+      <p className="font-hind font-normal text-[12px]">{count}</p>
+    </div>
+  );
+};
+
+const ActionItem = ({
+  src,
+  title,
+  icon,
+  status,
+  className,
+}: ActionItemProps) => {
+  return (
+    <div>
+      {icon ? (
+        <button>
+          <div className="flex justify-center">
+            <Image src={src} width={18} height={18} alt="" />
+          </div>
+          <p>{title}</p>
+        </button>
+      ) : (
+        <p className={`${className}`}>{status}</p>
+      )}
+    </div>
+  );
 };
 
 const MyVendor = () => {
   return (
-    <div className="flex">
+    <div className="flex gap-6">
       <SectionLeft />
       <SectionRight />
     </div>
