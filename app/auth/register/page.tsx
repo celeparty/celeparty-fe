d205545b35060cc6607063dae5611c75869dfad2
prasojopxl <prasojodesigner@gmail.com>
2025-01-09 15:01:23 +0700

    register

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -7,11 +7,13 @@ import { Textarea } from "@/components/ui/textarea"
 import { axiosData } from "@/lib/services";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const signUpSchema = z.object({
 	name: z.string().nonempty({ message: "Nama tidak boleh kosong" }),
+	username: z.string().nonempty({ message: "User Name tidak boleh kosong" }),
 	email: z.string().nonempty({ message: "Email tidak boleh kosong" }).email({ message: "Invalid email address" }),
 	phone: z.string().nonempty({ message: "No Telepon tidak boleh kosong" }),
 	address: z.string().nonempty({ message: "Alamat tidak boleh kosong" }),
@@ -32,10 +34,16 @@ const signUpSchema = z.object({
 });
 
 const Registration = () => {
+	const [message, setMessage] = useState(false);
+	const [errorMessage, setErrorMessage] = useState<string | boolean | null>(
+	  false
+	);
+  
 	const form = useForm<z.infer<typeof signUpSchema>>({
 		resolver: zodResolver(signUpSchema),
 		defaultValues: {
 			name: "",
+			username:"",
 			email: "",
 			phone: "",
 			address: "",
@@ -48,6 +56,7 @@ const Registration = () => {
 		const sendNow = async () => {
 			try {
 				const data = {
+					name: values.name,
 					username: values.name,
 					email: values.email,
 					phone: values.phone,
@@ -55,15 +64,16 @@ const Registration = () => {
 					password: values.password,
 				};
 		  
-				const response = await axiosData("POST", "/auth/local/register", data);	
-				console.log(response)			
-			} catch (error) {
-				console.error(error);				
+				const response = await axiosData("POST", "/api/auth/local/register", data);	
+				setErrorMessage(false);
+				setMessage(true);		
+			} catch (error:any) {
+				console.error("Error:", error?.response.data.error.message);
+				setErrorMessage(error?.response.data.error.message);		
 			}
 		}
 		sendNow()		
 		form.reset();
-		// alert("Selamat!, Proses Register Kamu Berhasil!!");
 	};
 	return (
 		<div>
@@ -71,45 +81,52 @@ const Registration = () => {
 			<div className="relative">
 				<Form {...form}>
 					<form onSubmit={form.handleSubmit(signUp)} className="flex flex-col gap-4">
-						<div className="relative">
+					<div className="relative">
 							<input type="text"
 								placeholder="Nama Lengkap"
-								className="text-black px-4 py-2 rounded-lg min-w-[270px]"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
 								{...form.register("name")}
+							/>
+						</div>						
+						<div className="relative">
+							<input type="text"
+								placeholder="User Name"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
+								{...form.register("username")}
 							/>
 						</div>						
 						<div className="relative">
 							<input type="email"
 								placeholder="Email"
-								className="text-black px-4 py-2 rounded-lg min-w-[270px]"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
 								{...form.register("email")}
 							/>
 						</div>						
 						<div className="relative">
 							<input type="text"
 								placeholder="No Telepon"
-								className="text-black px-4 py-2 rounded-lg min-w-[270px]"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
 								{...form.register("phone")}
 							/>
 						</div>						
 						<div className="relative">
 							<textarea
 								placeholder="Address"
-								className="text-black px-4 py-2 rounded-lg min-w-[270px]"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
 								{...form.register("address")}
 							/>
 						</div>						
 						<div className="relative">
 							<input type="password"
 								placeholder="Kata Sandi"
-								className="text-black px-4 py-2 rounded-lg min-w-[270px]"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
 								{...form.register("password")}
 							/>
 						</div>						
 						<div className="relative">
 							<input type="password"
 								placeholder="Ulangi Kata Sandi"
-								className="text-black px-4 py-2 rounded-lg min-w-[270px]"
+								className="text-black px-4 py-2 rounded-lg min-w-[270px] w-full"
 								{...form.register("confirmPassword")}
 							/>
 						</div>						
@@ -129,6 +146,14 @@ const Registration = () => {
 						</div>
 					</form>
 				</Form>
+				{message ? (
+					<div className="mt-1 text-green-500">
+					Terima kasih
+					</div>
+				) : null}
+				{errorMessage ? (
+					<div className="text-c-red mt-1">{errorMessage}</div>
+				) : null}
 			</div>
 		</div>
 	);
