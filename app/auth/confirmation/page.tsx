e39fd049378cccfa4fc369a2e661a97393b7a303
prasojopxl <prasojodesigner@gmail.com>
2025-01-12 20:45:18 +0700

    add static page

---
+++ app/auth/confirmation/page.tsx
@@ -0,0 +1,14 @@
+import Box from "@/components/Box"
+import React from 'react'
+
+export default function page() {
+  return (
+	<div className="relative wrapper-main py-7">
+		<div className="flex justify-between items-start gap-7">
+			<Box className=" w-full max-w-[600px] mx-auto mt-0">
+				Terima Kasih sudah mendaftar. Silahkan ke halaman login
+			</Box>
+		</div>
+	</div>
+  )
+}
