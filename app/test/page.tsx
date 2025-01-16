cc539a9cb7aa561b1ba38f1e3f5b3d6c7d1cb295
RikyAdiPutra <rikyadiputra76@gmail.com>
2025-01-16 14:15:25 +0700

    create new path /test for testing to new url api

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+const TestPageNewApi = () => {
+    console.log("ini console dari komponen TestPageNewApi")
+    return (
+        <div>HELLO WORLD!</div>
+    )
+}
+
+export default TestPageNewApi
