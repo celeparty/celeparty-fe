8ccbf8a7a49448f032ed317cb18d9fe4f5a53363
prasojopxl <prasojodesigner@gmail.com>
2025-01-17 14:06:03 +0700

    forgot password

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -23,20 +23,12 @@ import { axiosData } from "@/lib/services";
 
 const passwordSchema = z
 	.object({
-		password: z
+		email: z
 		.string()
-		.nonempty({ message: "Kata Sandi tidak boleh kosong" })
-		.min(8, "Kata sandi harus memiliki minimal 8 karakter")
-		.regex(/[A-Z]/, "Kata sandi harus mengandung setidaknya satu huruf besar")
-		.regex(/[a-z]/, "Kata sandi harus mengandung setidaknya satu huruf kecil")
-		.regex(/[0-9]/, "Kata sandi harus mengandung setidaknya satu angka"),
-		confirmPassword: z.string().nonempty({ message: "Konfirmasi kata sandi tidak boleh kosong" }),
-	})
-	.refine((data) => data.password === data.confirmPassword, {
-		path: ["confirmPassword"], // Arahkan pesan error ke field `confirmPassword`
-		message: "Konfirmasi kata sandi harus sama dengan kata sandi",
-	});
+		.nonempty({ message: "Email tidak boleh kosong" })
+		.email({ message: "Alamat email tidak valid" })
 
+	})
 export  function ResetPasswordPage() {
 	const [message, setMessage] = useState(false);
 	const router = useRouter();
@@ -49,8 +41,7 @@ export  function ResetPasswordPage() {
 		} = useForm<z.infer<typeof passwordSchema>>({
 			resolver: zodResolver(passwordSchema),
 			defaultValues: {
-				password: "",
-				confirmPassword: "",
+				email: ""
 			},
 		});
 
@@ -58,11 +49,9 @@ export  function ResetPasswordPage() {
 		const sendNow = async () => {
 			try {
 				const data = {
-					code: params.get("code"),
-					password: values.password,
-					passwordConfirmation: values.confirmPassword,
+					email: values.email,
 				}
-				const response = await axiosData("POST", "/api/auth/reset-password", data);	
+				const response = await axiosData("POST", "/api/auth/forgot-password", data);	
 				setMessage(true);		
 			} catch (error:any) {
 				console.error("Error:", error?.response.data.error.message);
@@ -75,24 +64,16 @@ export  function ResetPasswordPage() {
 		<div className="relative wrapper-main py-7">
 				<div className="flex justify-between items-start gap-7">
 				<Box className=" w-full max-w-[600px] mx-auto mt-0">
-				<h4 className="text-3xl font-bold mb-5 text-c-gray-text">Reset Kata Sandi</h4>
+				<h4 className="text-3xl font-bold mb-5 text-c-gray-text">Masukan email anda</h4>
 				<form onSubmit={handleSubmit(sendForgot)} className="flex flex-col gap-4">
 					<div className="relative">
-						<input type="password"
-							placeholder="Kata Sandi"
+						<input type="email"
+							placeholder="Email"
 							className="text-black border border-[#D9D9D9] px-4 py-2 rounded-lg min-w-[270px] w-full"
-							{...register("password")}
+							{...register("email")}
 						/>
-						{errors.password && <span className="text-red-500 text-xs">{errors.password.message}</span>}
-					</div>						
-					<div className="relative">
-						<input type="password"
-							placeholder="Ulangi Kata Sandi"
-							className="text-black border border-[#D9D9D9] px-4 py-2 rounded-lg min-w-[270px] w-full"
-							{...register("confirmPassword")}
-						/>
-						{errors.confirmPassword && <span className="text-red-500 text-xs">{errors.confirmPassword.message}</span>}
-					</div>					
+						{errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
+					</div>								
 					<div className="relative">
 						<Button className="h-[42px] text-center text-white rounded-full px-7">
 							Submit
@@ -101,7 +82,7 @@ export  function ResetPasswordPage() {
 				</form>
 				{message ? (
 					<div className="mt-1 text-green-500">
-					Password berhasil diubah
+						Link reset password telah dikirim ke email anda
 					</div>
 				) : null}
 
