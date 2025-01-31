cc1fb58ce0b5d1a4b6f567f8b1102510973e9991
RikyAdiPutra <rikyadiputra76@gmail.com>
2025-01-31 19:45:08 +0700

    blogDetail work with static slug ( * )

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -15,8 +15,8 @@ export default async function BlogDetail({
 	params: { slug: string };
 }) {
 	const slug = params.slug;
-	const dataBlog = await axiosData("GET",`/blogs/${slug}`);
-	const dataContent = dataBlog ? dataBlog.data?.data : null;
+	const dataBlog = await axiosData("GET",`/api/blogs/jry1s1641uyxngzd17jwe38k?populate=*`);
+	const dataContent = dataBlog ? dataBlog?.data : null;
 	return (
 		<div className="relative py-7">
 			<Box rounded={false}>
