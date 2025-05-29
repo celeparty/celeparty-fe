c050a8bf56e191f343d81a0db78074130e30f5ef
feFCProject <suteja.indrayana@flyingcape.com.sg>
2025-05-29 15:11:02 +0700

    Task 23: Make image clickable

---
+++ lib/interfaces/iPost.ts
@@ -0,0 +1,22 @@
+export interface iPostCat {
+  id: number;
+  documentId: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
+export interface iPostImage {
+  url: string;
+}
+export interface iBlogPost {
+  id: number;
+  documentId: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  content: string;
+  image: iPostImage | null;
+  category: iPostCat;
+}
