d130ff64243b51d0c97b3c50d39f920d68529c6a
feFCProject <suteja.indrayana@flyingcape.com.sg>
2025-05-29 20:03:37 +0700

    Task 15: Make formatter for number

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,12 +2,18 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export const formatRupiah = (val: number) => {
-	return new Intl.NumberFormat('id-ID', {
-	  style: 'currency',
-	  currency: 'IDR',
-	}).format(val);
-  };
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(val);
+};
+
+export const formatNumberWithDots = (value: string): string => {
+  const numberString = value.toString().replace(/\D/g, "");
+  if (!numberString) return "";
+  return numberString.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+};
