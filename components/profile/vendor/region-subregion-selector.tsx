68e718216f336ca3f0433f0c848a6ac154973619
feFCProject <suteja.indrayana@flyingcape.com.sg>
2025-05-31 09:26:11 +0700

    Fix region selection

--- components/profile/vendor/region-subregion-selector.tsx
+++ components/profile/vendor/region-subregion-selector.tsx
@@ -1,9 +1,9 @@
+import { SelectInput } from "@/components/form-components/SelectInput";
+import { iSelectOption } from "@/lib/interfaces/iCommon";
+import { iServiceLocation, iSubregionRes } from "@/lib/interfaces/iMerchant";
+import { axiosRegion } from "@/lib/services";
 import React, { useEffect, useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
-import _ from "lodash";
-import { iSelectOption } from "@/lib/interfaces/iCommon";
-import { axiosRegion } from "@/lib/services";
-import { SelectInput } from "@/components/form-components/SelectInput";
 
 interface RegionSubregionSelectorProps {
   index: number;
@@ -17,58 +17,91 @@ const RegionSubregionSelector: React.FC<RegionSubregionSelectorProps> = ({
   const { control, watch, setValue } = useFormContext();
   const [subregionOptions, setSubregionOptions] = useState<iSelectOption[]>([]);
 
-  const regionValue = watch(`serviceLocation.${index}.region`);
-
-  // useEffect(() => {
-  //   const fetchSubregion = async () => {
-  //     if (!regionValue) return;
-  //     try {
-  //       const response = await axiosRegion("GET", "kabupaten", regionValue);
-  //       setSubregionOptions(
-  //         (response?.value || []).map((item: any) => ({
-  //           label: item.name,
-  //           value: item.id,
-  //         }))
-  //       );
-  //       setValue(`serviceLocation.${index}.subregion`, "");
-  //     } catch (error) {
-  //       console.error("Error fetching subregions:", error);
-  //     }
-  //   };
-
-  //   fetchSubregion();
-  // }, [regionValue]);
+  const regionValue = watch(`serviceLocation.${index}.id`);
 
   useEffect(() => {
-    console.log(regionValue);
-  }, [provinceOptions]);
+    const fetchSubregion = async () => {
+      if (!regionValue) return;
+      try {
+        const response = await axiosRegion("GET", "kabupaten", regionValue);
+        setSubregionOptions(
+          (response?.value || []).map((item: iSubregionRes) => ({
+            label: item.name,
+            value: item.id,
+          }))
+        );
+      } catch (error) {
+        console.error("Error fetching subregions:", error);
+      }
+    };
+
+    fetchSubregion();
+  }, [regionValue]);
 
   return (
     <div className="flex gap-2 w-full">
       <div className="w-[50%] mb-2">
         <Controller
-          name={`serviceLocation.${index}.region`}
+          name={`serviceLocation.${index}.id`}
           control={control}
           render={({ field }) => (
-            <SelectInput
-              label="Provinsi"
-              onChange={field.onChange}
-              value={field.value}
-              options={provinceOptions}
-            />
+            <>
+              <SelectInput
+                label="Provinsi"
+                onChange={(id) => {
+                  let selectedProvince = provinceOptions.find(
+                    (pv) => pv.value === id
+                  );
+                  if (selectedProvince) {
+                    let updatedValue: iServiceLocation = {
+                      id,
+                      region: selectedProvince?.label || "",
+                      subregion: field.value?.subregion || "",
+                      idSubRegion: field.value?.idSubRegion || "",
+                    };
+                    field.onChange(updatedValue);
+                    setValue(`serviceLocation.${index}.id`, id);
+                    setValue(
+                      `serviceLocation.${index}.region`,
+                      selectedProvince?.label
+                    );
+                  }
+                }}
+                value={field.value || ""}
+                options={provinceOptions}
+              />
+            </>
           )}
         />
       </div>
 
       <div className="w-[50%] mb-2">
         <Controller
-          name={`serviceLocation.${index}.subregion`}
+          name={`serviceLocation.${index}.idSubRegion`}
           control={control}
           render={({ field }) => (
             <SelectInput
               label="Kabupaten"
-              onChange={field.onChange}
-              value={field.value}
+              onChange={(id) => {
+                let selectedRegion = subregionOptions.find(
+                  (sr) => sr.value === id
+                );
+                if (selectedRegion) {
+                  let updatedValue: iServiceLocation = {
+                    idSubRegion: id,
+                    subregion: selectedRegion.label,
+                    region: field.value?.region || "",
+                    id: field.value?.id || "",
+                  };
+                  field.onChange(updatedValue);
+                  setValue(`serviceLocation.${index}.idSubRegion`, id);
+                  setValue(
+                    `serviceLocation.${index}.subregion`,
+                    selectedRegion?.label
+                  );
+                }
+              }}
+              value={field.value || ""}
               options={subregionOptions}
             />
           )}
