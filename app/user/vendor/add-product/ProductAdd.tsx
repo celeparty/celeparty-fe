aa487c5f8711da7b63cd6f91c549d36faee72641
prasojopxl <prasojodesigner@gmail.com>
2025-07-17 13:06:19 +0700

    rev:layout add product vendor

---
+++ app/user/vendor/add-product/ProductAdd.tsx
@@ -0,0 +1,46 @@
+"use client"
+import React from 'react'
+import { ProductForm } from "@/components/product/ProductForm";
+import {
+  iProductImage,
+  iProductReq,
+  iProductVariant,
+} from "@/lib/interfaces/iProduct";
+import Box from "@/components/Box";
+
+export default function ProductAdd() {
+  const defaultFormData: iProductReq = {
+    title: "",
+    description: "",
+    main_price: "0",
+    minimal_order: 0,
+    main_image: [{} as iProductImage],
+    price_min: "0",
+    price_max: "0",
+    category: {
+      connect: 0,
+    },
+    kabupaten: "",
+    rate: 0,
+    minimal_order_date: "",
+    users_permissions_user: {
+      connect: {
+        id: "",
+      },
+    },
+    variant: [{} as iProductVariant],
+    escrow: false,
+  };
+
+  return (
+    <div>
+      <Box className="lg:mt-0 rounded-t-none">
+        <ProductForm
+          isEdit={false}
+          formDefaultData={defaultFormData}
+        ></ProductForm>
+      </Box>
+      
+    </div>
+  )
+}
---
+++ app/user/vendor/add-product/TicketAdd.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TicketAdd() {
+  return (
+	<div>TicketAdd</div>
+  )
+}
