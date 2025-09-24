0a350fef25129fe06944dfef265e44c7a5fd70d5
Bepe_ <pujon.ken2@gmail.com>
2025-09-25 01:00:41 +0700

    Update MainBanner.tsx

--- components/MainBanner.tsx
+++ components/MainBanner.tsx
@@ -25,11 +25,11 @@ const settings = {
 	arrow: true,
 	dots: true,
 	infinite: true,
-	speed: 500,
+	speed: 1000,
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	autoplay: true,            // aktifkan auto play
-    autoplaySpeed: 5000,		// delay per slide (5 detik)
+    autoplaySpeed: 7000,		// delay per slide (5 detik)
     cssEase: "linear",      
     pauseOnHover: true,        // berhenti kalau kursor di atas
 };
