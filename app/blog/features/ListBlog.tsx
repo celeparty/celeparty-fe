f81ac98a31621bd30507a34009dbfb97d3bba963
Bepe_ <pujon.ken2@gmail.com>
2025-09-25 00:45:53 +0700

    Update ListBlog.tsx

--- app/blog/features/ListBlog.tsx
+++ app/blog/features/ListBlog.tsx
@@ -40,7 +40,7 @@ export default function ListBlog() {
 							key={index}
 							title={item?.title}
 							date={moment(item?.publish_at).format("DD MMM YYYY")}
-							image={item?.image?.url ? `https://sub.typestaging.my.id${item.image.url}` : "/images/noimage.png"}
+							image={item?.image?.url ? `${process.env.BASE_API}${item?.image?.url}` : "/images/noimage.png"}
 						/>
 					);
 				})}
