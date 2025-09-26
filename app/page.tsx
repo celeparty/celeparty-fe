fd076f04bcb15b05e03e538724d255262726ce54
Bepe_ <pujon.ken2@gmail.com>
2025-09-26 23:06:36 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import EventList from "@/components/EventList";
 import MainBanner from "@/components/MainBanner";
 import { ProductList } from "@/components/product/ProductList";
 
+export const metadata = {
+  title: "Beranda", // hasil akhir = "Beranda | Celeparty"
+  description: "Temukan event seru dan sewa peralatan event terbaik di Celeparty.",
+};
+
 export default function Home() {
   return (
     <div className="relative wrapper py-7">
