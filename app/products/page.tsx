af268fdee7db5dd7aba06c6c5853a5bbb1ab9697
Bepe_ <pujon.ken2@gmail.com>
2025-09-26 23:09:57 +0700

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,6 +2,11 @@ import Basecontent from "@/components/Basecontent";
 import React from "react";
 import ProductContent from "./ProductContent_";
 
+export const metadata = {
+  title: "Produk Terbaik Kami", // hasil akhir = "Beranda | Celeparty"
+  description: "Temukan Tiket Event dan Sewa Peralatan untuk Acara yang Profesional ",
+};
+
 export default function ProductPage() {
 	return (
 		<div className="relative wrapper-main lg:py-7 py-0 lg:px-9 px-[10px]">
