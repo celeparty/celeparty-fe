cc771d47c2df6232eb5859dbb8f0073449e07041
Bepe_ <pujon.ken2@gmail.com>
2025-09-26 23:11:55 +0700

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,11 @@ import React from "react";
 import ListBlog from "./features/ListBlog";
 import MainBlog from "./features/MainBlog";
 
+export const metadata = {
+  title: "Blog", // hasil akhir = "Beranda | Celeparty"
+  description: "Artikel menarik mengenai Event dan Rahasia di balik kesuksesan Sebuah Acara / Event",
+};
+
 export default function BlogPage() {
 	return (
 		<div className="relative wrapper lg:py-7 py-0">
