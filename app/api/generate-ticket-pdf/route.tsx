5bcc09ee35be173b10c017b54e7b5b8685ac6d99
Bepe_ <pujon.ken2@gmail.com>
2025-12-21 08:56:11 +0700

    Update route.tsx

--- app/api/generate-ticket-pdf/route.tsx
+++ app/api/generate-ticket-pdf/route.tsx
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import puppeteer from "puppeteer";
-import { ReactDOMServer } from "react-dom/server";
+import ReactDOMServer from "react-dom/server";
 import TicketTemplate from "@/components/ticket-templates/TicketTemplate"; // This component needs to be created
 
 // Helper function to get a browser instance
