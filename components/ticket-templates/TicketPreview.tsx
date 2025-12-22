bf3ba5e81c0bb615219d30d5e3b367d6c52f161d
Bepe_ <pujon.ken2@gmail.com>
2025-12-23 00:05:53 +0700

    Update TicketPreview.tsx

--- components/ticket-templates/TicketPreview.tsx
+++ components/ticket-templates/TicketPreview.tsx
@@ -7,7 +7,8 @@
 
 import React, { useRef, useState } from 'react';
 import { TicketTemplate } from '@/components/ticket-templates';
-import type {TicketData , iTicketTemplateConfig } from '@/components/ticket-templates';
+import type { TicketData }from '@/components/ticket-templates';
+import type { iTicketTemplateConfig } from '@/lib/utils/ticket-template/configTemplate';
 import { downloadTicketPDF, getTicketPDFAsBase64 } from '@/lib/utils/ticket-template';
 import { getDefaultTemplateConfig } from '@/lib/utils/ticket-template/configTemplate';
 import toast from 'react-hot-toast';
