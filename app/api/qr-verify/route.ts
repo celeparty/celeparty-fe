70711ecb21bfb51d7a4618d6c9c883376d2d0766
Celeparty.Git <celeparty.id@gmail.com>
2026-04-17 15:46:07 +0700

    route update

--- app/api/qr-verify/route.ts
+++ app/api/qr-verify/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth/options";
-import { authOptions } from "@/app/auth";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export const dynamic = "force-dynamic";
 
